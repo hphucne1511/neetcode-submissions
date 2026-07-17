95585c94781d8e496067defd72a1c78252cec323
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-07-17 15:39:42 +0700

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> container;
+        for(string s : strs){
+            vector<int> count(26, 0);
+            for(char c : s){
+                ++count[c - 'a'];
+            }
+            string key = "";
+            for(int i = 0; i < 26; ++i){
+                key += "#" + to_string(count[i]); 
+            }
+            container[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto& pair : container){
+            ans.push_back(pair.second);
+        }
+        return ans;
+    }
+};
