b3fdd6afe5a1599f3dadaf1a535e37045638b0ae
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-07-18 13:49:58 +0700

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for(int num : nums){
+            ++count[num];
+            if(count[num] > nums.size()/2) return num;
+        } 
+        return nums[0];
+    }
+};
