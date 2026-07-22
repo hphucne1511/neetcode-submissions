1c71804d5d92c0f87fcd1d58dbf54b045d5b2547
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-07-22 14:50:06 +0700

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        //insertionSort
+        for(int i = 0; i < nums.size();++i){
+            int min = nums[i];
+            int j = i;
+            int lIdx = j;
+            while(j < nums.size()){
+                if(min > nums[j]){
+                    min = nums[j];
+                    lIdx = j;
+                }
+                j++;
+            }
+            int temp = nums[i];
+            nums[i] = nums[lIdx];
+            nums[lIdx] = temp;
+        }
+        return nums;
+    }
+};
