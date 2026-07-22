d18dcc8ee4614549f8646787e5c5b16f0d6b7a1a
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-07-22 22:41:05 +0700

    Add: sort-an-array - submission-17

---
+++ Data Structures & Algorithms/sort-an-array/submission-17.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        // straight insertion sort
+        /*
+        for(int i = 1; i < nums.size();++i){
+            int key = nums[i];
+            int j = i - 1;
+            while(j >= 0 && nums[j] > key){
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+            
+        }
+        */
+        //Bubble sort
+        for(int i = 0; i < nums.size();i++){
+            for(int j = nums.size() - 1; j > i;j--){
+                if(nums[j - 1] > nums[j]) swap(nums[j], nums[j - 1]);
+            }
+        }
+        return nums;
+    }
+};
