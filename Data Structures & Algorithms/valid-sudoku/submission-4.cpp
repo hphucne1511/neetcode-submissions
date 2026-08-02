016e94c9b239814bae614d59dd786abd5d4070c1
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-08-02 14:15:37 +0700

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9] = {0};
+        int cols[9] = {0};
+        int square[9] = {0};
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.') continue;
+                int val = board[i][j] - '1';
+                if((rows[i] & (1 << val)) || (cols[j] & (1 << val)) 
+                || square[(i / 3) * 3 + j / 3] & (1 << val)){
+                    return false;
+                }
+                rows[i] |= (1 << val);
+                cols[j] |= (1 << val);
+                square[(i / 3) * 3 + j / 3] |= (1 << val);
+            }
+        }
+        return true;
+    }
+};
