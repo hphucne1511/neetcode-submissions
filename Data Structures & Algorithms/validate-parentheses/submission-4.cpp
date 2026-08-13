a9f46a2c07f09e940631396347d619fac8a13599
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-08-13 14:43:12 +0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> cto = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        for(char c : s){
+            if(cto.count(c)){
+                if(!st.empty() && st.top() == cto[c]) st.pop();
+                else return false;
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.empty();
+    } 
+};
