8c9729b517adb31df3deb73cfed3d8fed5db2f6f
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-08-14 20:55:29 +0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+private:
+    stack<int> s1, s2;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s1.push(val);
+        if(s2.empty()) s2.push(val);
+        else{
+            s2.push(min(s2.top(), val));
+        }
+    }
+    
+    void pop() {
+        s1.pop();
+        s2.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
