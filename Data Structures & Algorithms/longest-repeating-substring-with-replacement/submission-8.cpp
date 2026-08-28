52a7b7cac74a16d0c031bd1b5536735397e4a79d
Ông Hoàng Phúc <onghoangphuc1511@gmail.com>
2026-08-28 18:20:24 +0700

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_set<char> us(s.begin(), s.end());
+        int res = 0;
+        for(char c : us){
+            int l = 0, count = 0;
+            for(int r = 0; r < s.size(); r++){
+                if(s[r] == c) count++;
+                while(r - l + 1 - count > k){
+                    if(s[l] == c) count--;
+                    l++;
+                }
+                res = max(res, r - l + 1);
+            }
+        }
+        return res;
+    }
+};
